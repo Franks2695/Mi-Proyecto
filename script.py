534fdb178b405aa98846370b41104d17e65b88d6
Franks Sañay <fsanays@est.ups.edu.ec>
2020-12-30 16:44:07 -0500

    Agregado datos del proyecto

---
+++ script.py
@@ -0,0 +1,12 @@
+# Script de prueba para Git
+# Autor: Franks Sañay <fsanays@est.ups.edu.ec>
+# Fecha: 30-12-2020
+# Version: 1.0.0
+
+def main():
+    nombre = input("Cual es tu nombre? ")
+    print("Hola", nombre, "Bienvenido a Git!")
+
+
+if __name__ == "__main__";
+    main()
